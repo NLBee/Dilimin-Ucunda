28e54a660404e62ec664bd710db57fb717a4aaef
kutaygallo <kutaygallo@gmail.com>
2022-08-04 20:56:48 +0300

    scraper modülünde minör değişiklikler yapıldı.

--- beyazperde dataset/scraper.py
+++ beyazperde dataset/scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 URL = "https://www.beyazperde.com/filmler-tum/kullanici-puani/"
-NUM_OF_PAGES = 1
+NUM_OF_PAGES = 228
 
 def page_link_generator():
     page_urls = [URL]
